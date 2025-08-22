ee99b496ecf4d3d7b96ed7f62f6ca8c285d9bea0
michael ramboer <michaelramboer123@gmail.com>
2025-08-22 14:59:16 +0200

    maj

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import numpy as np
 '''
 # 🚕 TaxiFare
 '''
-
-
 '''
 📍 Rides Parameters
 '''
@@ -23,33 +21,23 @@ time = st.time_input('What time is it ', datetime.time(8, 45))
 pickup_datetime = f"{date} {time}"
 
 #pickup longitude
-pickup_longitude = st.number_input('Pickup longitude')
+pickup_longitude = st.number_input('Pickup longitude',format="%.6f",value=-73.950655)
 
 # pickup latitude
-pickup_latitude = st.number_input('Pickup latitude')
+pickup_latitude = st.number_input('Pickup latitude',format="%.6f", value=40.783282)
 
 #dropoff longitude
-dropoff_longitude = st.number_input('Dropoff longitude')
+dropoff_longitude = st.number_input('Dropoff longitude',format="%.6f",value=-73.984365)
 
 # dropoff latitude
-dropoff_latitude = st.number_input('Dropoff latitude')
+dropoff_latitude = st.number_input('Dropoff latitude',format="%.6f",value=40.769802)
 
 # passenger count
-passenger_count = st.number_input('Passenger count',step=0,format="%i")
+passenger_count = st.number_input('Passenger count',step=0,format="%i",value=1)
 
 
 url = 'https://taxifare.lewagon.ai/predict'
 
-
-def get_map_data():
-    return pd.DataFrame(
-        {
-            'lat': [dropoff_latitude],
-            'lon': [dropoff_longitude]
-        }
-    )
-df = get_map_data()
-st.map(df)
 
 
 #dict parameters API
@@ -63,4 +51,19 @@ dico_params = {"pickup_datetime" : [pickup_datetime],
 
 prediction = requests.get(url=url, params=dico_params).json()
 
-st.write('The prediction is $', round(prediction['fare'],2))
+if st.button('💲Prediction price'):
+    st.write('The prediction price is $',round(prediction['fare'],2))
+
+
+
+def get_map_data():
+    return pd.DataFrame(
+        {
+            'lat': [dropoff_latitude],
+            'lon': [dropoff_longitude]
+        }
+    )
+df = get_map_data()
+
+if st.button('🗺️ Map'):
+    st.map(df)
